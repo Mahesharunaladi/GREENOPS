c2cf87f334fc9ee98226327431a823daa757ef99
Mahesharunaladi <mahesharunaladi@gmail.com>
2026-08-17 11:47:38 +0530

    BACKEND

--- BACKEND/src/main/java/com/greenops/scrapper/config/GreenOpsProperties.java
+++ BACKEND/src/main/java/com/greenops/scrapper/config/GreenOpsProperties.java
@@ -2,7 +2,6 @@ package com.greenops.scrapper.config;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "greenops")
