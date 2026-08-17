f2fb04d8d1c90712e598262a008d78342d542ef2
Mahesharunaladi <mahesharunaladi@gmail.com>
2026-08-17 11:48:16 +0530

    BACKEND

--- BACKEND/src/main/java/com/greenops/scrapper/GreenOpsScrapperApplication.java
+++ BACKEND/src/main/java/com/greenops/scrapper/GreenOpsScrapperApplication.java
@@ -1,6 +1,5 @@
 package com.greenops.scrapper;
 
-import com.greenops.scrapper.config.GreenOpsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
