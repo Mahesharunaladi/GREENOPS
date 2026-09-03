e90ae7b66962792cf7c116dafeadb922767207e1
Mahesharunaladi <mahesharunaladi@gmail.com>
2026-09-03 22:20:06 +0530

    Fix-frontend and main.tf

--- BACKEND/terraform/main.tf
+++ BACKEND/terraform/main.tf
@@ -12,9 +12,9 @@ Principal = { Service = "lambda.amazonaws.com" }
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_basic" {
-name       = "GreenOpsLambdaBasic"
-roles      = [aws_iam_role.lambda_role.name]
-policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+ Name       = "GreenOpsLambdaBasic"
+Roles      = [aws_iam_role.lambda_role.name]
+Policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 resource "aws_iam_policy" "greenops_policy" {
 name   = "GreenOpsLambdaPolicy"
