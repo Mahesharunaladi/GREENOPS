e3c7f1fc5bd1616d29875cb4cd3922cf66f5cc74
Mahesharunaladi <mahesharunaladi@gmail.com>
2026-09-17 02:07:20 +0530

    updated GreenOPs Scrpper Application

--- BACKEND/src/main/java/com/greenops/scrapper/GreenOpsScrapperApplication.java
+++ BACKEND/src/main/java/com/greenops/scrapper/GreenOpsScrapperApplication.java
@@ -1,12 +1,19 @@
 package com.greenops.scrapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class GreenOpsScrapperApplication {
+
+    @Bean
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GreenOpsScrapperApplication.class, args);
